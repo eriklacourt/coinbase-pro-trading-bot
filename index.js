4d390a2a6fb859d314a8269a72557b8660d295b3
eriklacourt <65132457+eriklacourt@users.noreply.github.com>
2021-09-30 20:52:38 -0400

    coinbase connection

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const {CoinbasePro} = require('coinbase-pro-node');
-const client = new CoinbasePro();
 
-console.log("Running on pi!");
+console.log("$$let's print money$$");
+
+const auth = {
+    apiKey: process.env.PROD ? process.env.PROD_API_KEY : process.env.SANDBOX_API_KEY,
+    apiSecret: process.env.PROD ? process.env.PROD_API_SECRET : process.env.SANDBOX_API_SECRET,
+    passphrase: process.env.PROD ? process.env.PROD_PASSPHRASE : process.env.SANDBOX_PASSPHRASE,
+    // The Sandbox is for testing only and offers a subset of the products/assets:
+    // https://docs.pro.coinbase.com/#sandbox
+    useSandbox: process.env.SANDBOX,
+};
+
+const client = new CoinbasePro(auth);
+
+client.rest.account.listAccounts().then(accounts => {
+    const message = `You can trade "${accounts.length}" different pairs.`;
+    console.log(message);
+});
