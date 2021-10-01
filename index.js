65ff6e69478c200ede6e15f4d622c3c244822914
eriklacourt <65132457+eriklacourt@users.noreply.github.com>
2021-09-30 21:30:57 -0400

    incremental

--- index.js
+++ index.js
@@ -2,32 +2,27 @@ const {CoinbasePro} = require('coinbase-pro-node');
 
 console.log("$$let's print money$$");
 
+const apiURI = 'https://api.pro.coinbase.com';
+const sandboxURI = 'https://api-public.sandbox.pro.coinbase.com';
+
+const myCallback = (err, response, data) => {
+    if (err) {
+        console.log(err);
+      } else {
+        console.log(response);
+        console.log(data);
+      }
+  };
+
 const auth = {
     apiKey: process.env.PROD ? process.env.PROD_API_KEY : process.env.SANDBOX_API_KEY,
     apiSecret: process.env.PROD ? process.env.PROD_API_SECRET : process.env.SANDBOX_API_SECRET,
     passphrase: process.env.PROD ? process.env.PROD_PASSPHRASE : process.env.SANDBOX_PASSPHRASE,
     // The Sandbox is for testing only and offers a subset of the products/assets:
     // https://docs.pro.coinbase.com/#sandbox
-    useSandbox: process.env.SANDBOX,
+    useSandbox: process.env.SANDBOX ? sandboxURI : apiURI,
 };
 
-const client = new CoinbasePro(auth);
+const authedClient = new CoinbasePro.AuthenticatedClient(auth);
 
-const channel = {
-    name: WebSocketChannelName.USER,
-    product_ids: ['BTC-USD'],
-  };
-  
-  client.ws.on(WebSocketEvent.ON_MESSAGE, message => {
-    console.info(`Received message of type "${message.type}".`, message);
-  });
-  
-  client.ws.on(WebSocketEvent.ON_MESSAGE_ERROR, errorMessage => {
-    throw new Error(`${errorMessage.message}: ${errorMessage.reason}`);
-  });
-  
-  client.ws.on(WebSocketEvent.ON_OPEN, () => {
-    client.ws.subscribe(channel);
-  });
-  
-  client.ws.connect();
+const result = publicClient.getProducts(myCallback);
